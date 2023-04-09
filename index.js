7ec9fef2282667db32158204d72dd3babc0ed8eb
Leafeling <morgan@morganhofmann.nl>
2023-04-09 20:56:04 +0200

    Socket connections and YouTube player provider

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const { createServer } = require('http');
+
 let app = express();
 
 let port = 3000;
+let config = require("./config.json");
 
 app.use(express.json({}));
 app.use(express.static("./static/"));
 app.use(express.static("./assets/"));
 
-app.get("/config", (req, res) => { res.json(require("./config.json")); });
+app.get("/config", (req, res) => { res.json(config); });
 
-app.listen(port, () => {
+let httpServer = createServer(app);
+
+const { Server } = require('socket.io');
+let io = new Server(httpServer, {});
+
+io.on("connection", (socket) => {
+    
+});
+
+httpServer.listen(port, () => {
     console.log(`Listening on port ${port}...`);
 });
--- static/js/jukebox.js
+++ static/js/jukebox.js
@@ -1,29 +1,92 @@
-const loadConfig = async () => {
-    await fetch("/config").then((res) => {
-        res.json().then((config) => {
-            document.title = config.name;
-            
-            if (!!config.icon) {
-                let iconLink = document.createElement('link');
-                iconLink.rel = 'icon';
-                iconLink.href = config.icon;
-                document.head.appendChild(iconLink);
-            }
+const getProviderUrl = async (provider) => {
+    switch (provider.toLowerCase()) {
+        case "youtube": return "https://www.youtube.com/embed/$songId";
+        default: return "";
+    }
+}
 
-            if (!!config.logo) {
-                document.querySelectorAll(".__config-logo").forEach((logo) => logo.setAttribute("src", config.logo));
-            }
+let cfg = {};
+const loadConfig = async () => {
+    let response = await fetch("/config");
+    let config = await response.json();
+    cfg = config;
+    document.title = config.name;
             
-            if (!!config.theme) {
-                document.body.style.setProperty("--theme-background", config.theme["background"]);
-                document.body.style.setProperty("--theme-navbar", config.theme["navbar"]);
-                document.body.style.setProperty("--theme-text-light", config.theme["text-light"]);
-                document.body.style.setProperty("--theme-text", config.theme["text"]);
+    if (!!config.icon) {
+        let iconLink = document.createElement('link');
+        iconLink.rel = 'icon';
+        iconLink.href = config.icon;
+        document.head.appendChild(iconLink);
+    }
+
+    if (!!config.logo) {
+        document.querySelectorAll(".__config-logo").forEach((logo) => logo.setAttribute("src", config.logo));
+    }
+            
+    if (!!config.theme) {
+        document.body.style.setProperty("--theme-background", config.theme["background"]);
+        document.body.style.setProperty("--theme-navbar", config.theme["navbar"]);
+        document.body.style.setProperty("--theme-text-light", config.theme["text-light"]);
+        document.body.style.setProperty("--theme-text", config.theme["text"]);
+    }
+}
+
+const connectSocket = async () => {
+    const socket = io();
+}
+
+let songId = "";
+const changeSongId = async (id) => {
+    songId = id;
+    setPlayerSource(id);
+}
+
+const setPlayerSource = async (src) => {
+    if (!src) document.querySelector("#player").classList.add("hidden");
+    else {
+        if (!!ytPlayer) ytPlayer.loadVideoById(src);
+        document.querySelector("#player").classList.remove("hidden");
+    }
+}
+
+const loadPlayer = async (provider) => {
+    switch (provider) {
+        case "youtube":
+            // Initialize YouTube Iframe API.
+            var tag = document.createElement('script');
+
+            tag.src = "https://www.youtube.com/iframe_api";
+            var firstScriptTag = document.getElementsByTagName('script')[0];
+            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+            break;
+        default: return;
+    }
+}
+
+const startPlayer = async () => {
+    if (!!ytPlayer) ytPlayer.playVideo();
+}
+
+let ytPlayer;
+function onYouTubeIframeAPIReady() {
+    ytPlayer = new YT.Player('player', {
+        playerVars: {
+            'playsinline': 1
+        },
+        events: {
+            'onReady': (e) => {
+                e.target.playVideo();
+                e.target.setVolume(100);
+
+                ytPlayer = e.target;
+                setPlayerSource(null);
             }
-        });
-    });
+        }
+    })
 }
 
 window.addEventListener("load", async () => {
     await loadConfig();
+    await loadPlayer(cfg.provider);
+    await connectSocket();
 });
